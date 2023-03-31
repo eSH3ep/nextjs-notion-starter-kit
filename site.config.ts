d14db54781a35d006a8d3667ee9c55ad48a0a1d7
eSH3ep <66587871+eSH3ep@users.noreply.github.com>
2023-04-01 06:42:48 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7ff1afbc8d994441a898a8451e0dca99',
+  rootNotionPageId: '6d9eda71bb084fea8d0222eaa9550081',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -17,9 +17,6 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: '.',
-  github: '.',
-  linkedin: '.',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
