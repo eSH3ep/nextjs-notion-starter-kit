754f9656c3421593fc0f95b64b7e256cf8d239ca
eSH3ep <66587871+eSH3ep@users.noreply.github.com>
2023-04-02 03:11:14 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,8 +9,8 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: '烈火魔研社',
-  domain: 'https://www.notion.so/6d9eda71bb084fea8d0222eaa9550081?pvs=4',
+  name: 'blazingD',
+  domain: 'https://wooded-hydrant-a06.notion.site/6d9eda71bb084fea8d0222eaa9550081',
   author: 'Blazingdoggy',
 
   // open graph metadata (optional)
